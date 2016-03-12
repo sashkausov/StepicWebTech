0d3ef2950b5de31812028e220612b27202edd796
sashkausov <rs.alexandr@gmail.com>
2016-03-13 00:26:42 +0800

    sec

---
+++ hello.py
@@ -0,0 +1,11 @@
+from cgi import parse_qs
+
+def app(environ, start_response):
+	status = '200 OK'
+	headers = [
+		('Content-Type', 'text/plain')
+	]
+	start_response(status, headers )
+	qs = parse_qs(environ['QUERY_STRING'])
+	return ['%s=%s<br>' % (k, qs[k][0]) for k in qs ]
+
